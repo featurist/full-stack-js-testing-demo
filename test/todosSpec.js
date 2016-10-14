124123b36cf48b4007ac533f4f759366a52e05a3
artemave <artemave@gmail.com>
2016-10-14 17:56:32 +0100

    Test "Loading..."
    To demonstrate how bm deals with async dom change

--- test/todosSpec.js
+++ test/todosSpec.js
@@ -8,21 +8,35 @@ describe('todos app', () => {
     fetchTODOs: function() {
       return this.find('button').click();
     },
+
+    observeLoadingBar: function() {
+      return this.find('div', {text: 'Loading...'});
+    },
+
     expectTODOs: async function(...expectedTodos) {
-      var actualTodos = (await this.find('ul li').elements()).map(e => e.innerText);
+      var actualTodos = (await this.find('ul li').elements())
+        .map(e => e.innerText);
+
       expect(actualTodos.sort()).to.eql(expectedTodos.sort());
     }
   });
 
   beforeEach(() => {
-    var api = {};
-    api.loadTODOs = () => Promise.resolve([{title: 'one'}, {title: 'two'}]);
+    var fakeApi = {};
+    fakeApi.loadTODOs = () => {
+      return new Promise(function(resolve) {
+        setTimeout(() => {
+          resolve([{title: 'one'}, {title: 'two'}]);
+        }, 50);
+      });
+    }
 
-    mountApp(new App(api));
+    mountApp(new App(fakeApi));
   });
 
   it('fetches todos', async () => {
     await page.fetchTODOs();
+    await page.observeLoadingBar();
     await page.expectTODOs('one', 'two');
   });
 });
