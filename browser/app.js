f7ed35d053ef19340e83570453be730017c6da47
artemave <artemave@gmail.com>
2017-02-03 20:16:14 +0000

    jsx -> hyperx
    To not rely on babel

---
+++ browser/app.js
@@ -0,0 +1,48 @@
+const hyperdom = require('hyperdom')
+const hyperx = require('hyperx')
+const h = hyperx(hyperdom.html)
+
+const router = require('hyperdom-router')
+const httpism = require('httpism')
+
+function navigateTo(route) {
+  route().push();
+}
+
+module.exports = class App {
+  constructor(serverUrl) {
+    router.clear()
+    router.start()
+
+    this.api = httpism.api(serverUrl)
+
+    this.routes = {
+      home: router.route('/'),
+      todos: router.route('/todos')
+    };
+  }
+
+  async loadTODOs() {
+    this.todos = (await this.api.get('/api/todos')).body
+  }
+
+  render() {
+    return h`
+      <main>
+        ${
+          this.routes.home(() => {
+            return h`<button onclick=${ () => navigateTo(this.routes.todos) }>Fetch me TODOs</button>`
+          })
+        }
+        ${
+          this.routes.todos({ onarrival: () => this.loadTODOs() }, () => {
+            return this.todos
+              ?
+                h`<ul>${ this.todos.map(t => h`<li>${ t.title }</li>`) }</ul>`
+              :
+                h`<div class="loading">Loading...</div>`
+          })
+        }
+      </main>`
+  }
+}
